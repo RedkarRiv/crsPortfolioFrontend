8bb1c84ca56036e190dd5fbc6d74116d096bf576
Carlos <carresan.val@gmail.com>
2023-10-13 15:55:30 +0200

    FIX: Configurado REDUX

--- src/app/store.js
+++ src/app/store.js
@@ -11,7 +11,8 @@ const reducers = combineReducers({
 });
 
 const persistConfig = {
-  key: "root"
+  key: "root",
+  storage,
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
