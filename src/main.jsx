d036c2c04d2df0ecfbbbfdc9e9b212203ea1e774
Carlos <carresan.val@gmail.com>
2023-10-14 17:45:03 +0200

    FIX: Añadida importación de bootstrap.css: import bootstrap/dist/css/bootstrap.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import store from "./app/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 import { Provider } from "react-redux";
+import Navbar from "./common/Navbar/Navbar.jsx";
 
 const persistor = persistStore(store);
 
@@ -15,6 +17,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
+          <Navbar />
           <App />
         </BrowserRouter>
       </PersistGate>
