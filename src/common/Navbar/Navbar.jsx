6091f2f53d9b87389fe52ce34dfa98d262867249
Carlos <carresan.val@gmail.com>
2023-10-15 15:36:59 +0200

    NEW: Logo checking en navbar

--- src/common/Navbar/Navbar.jsx
+++ src/common/Navbar/Navbar.jsx
@@ -3,6 +3,9 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./Navbar.css";
 import AccountAvatar from "../../img/avatarAccount.png";
+import crsLOGO2 from "../../img/crsLogo2.png";
+import crsLOGO4 from "../../img/crsLogo4.png";
+import crsLOGO5 from "../../img/crsLogo5.png";
 
 export const NavbarTop = () => {
   return (
@@ -10,11 +13,13 @@ export const NavbarTop = () => {
       <Navbar
         collapseOnSelect
         expand="lg"
-        className="bg-primary text-white
+        className="navbarDesign text-white
         "
       >
         <Container className="">
-          <Navbar.Brand href="#home">CRedsan</Navbar.Brand>
+          <Navbar.Brand href="#home" className="ps-2">
+            <img src={crsLOGO5} alt="Mi cuenta" className="crsLOGODesign" />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto w-100 d-flex justify-content-center align-items-center">
@@ -31,14 +36,17 @@ export const NavbarTop = () => {
               <div className="p-1">Login</div>
               <div className="p-1">Registro</div>
             </Nav>
-            <Nav className="profileButtons w-auto text-light d-flex justify-content-center align-items-center text-center">
+            {/* <Nav className="profileButtons w-auto text-light d-flex justify-content-center align-items-center text-center">
               <div className="p-1">
                 {" "}
-                <img src={AccountAvatar} alt="Mi cuenta" className="avatarAccoutDesign"/>
+                <img
+                  src={AccountAvatar}
+                  alt="Mi cuenta"
+                  className="avatarAccoutDesign"
+                />
               </div>
               <div className="p-1">Perfil</div>
-
-            </Nav>
+            </Nav> */}
           </Navbar.Collapse>
         </Container>
       </Navbar>
