aeeb19463e2fc43846420d57aae04233f129b2d0
Carlos <carresan.val@gmail.com>
2023-10-15 19:29:54 +0200

    NEW: Implementado guardado en REDUX de login y creado componen LoginForm con la consecuente exportación desde Home del código

---
+++ src/common/LoginForm/LoginForm.jsx
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import { loginMe } from "../../services/apiCall";
+import jwt_decode from "jwt-decode";
+import { InputLabel } from "../../common/InputLabel/InputLabel";
+import { CheckError } from "../../services/useful";
+import { Button } from "../../common/Button/Button";
+import "./LoginForm.css";
+import { login } from "../../pages/userSlice";
+import { useDispatch } from "react-redux";
+
+export const LoginForm = () => {
+    const dispatch = useDispatch();
+
+  //CHECK FIELDS
+  const [errorMessage, setErrorMessage] = useState("");
+  const [credentialsError, setCredentialsError] = useState({
+    emailError: "",
+    passwordError: "",
+  });
+
+  const InputCheck = (e) => {
+    let mensajeError = CheckError(e.target.name, e.target.value);
+    console.log("Esto son las credenciatlsError");
+    console.log(credentialsError);
+    setCredentialsError((prevState) => ({
+      ...prevState,
+      [e.target.name + "Error"]: mensajeError,
+    }));
+  };
+
+  //LOGIN
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  const InputHandler = (e) => {
+    setCredentials((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+    console.log(credentials);
+  };
+
+  const logMe = (e) => {
+    e.preventDefault();
+
+    loginMe(credentials)
+      .then((resultado) => {
+        let decoded = jwt_decode(resultado.data.token);
+        let datosBackend = {
+          token: resultado.data.token,
+          user: decoded,
+        };
+        console.log(datosBackend);
+        dispatch(login({ credentials: datosBackend }));
+        setErrorMessage("");
+      })
+      .catch((error) => {
+        console.log(error.response.data.message);
+        setErrorMessage(error.response.data.message);
+      });
+  };
+
+  return <>
+  
+  <div className="formulario">
+          <div className="titulo">LOGIN</div>
+          <div className="nombrecampo">Usuario</div>
+          <InputLabel
+            type="text"
+            placeholder="Introduce tu email"
+            name="email"
+            classDesign=""
+            functionHandler={(e) => InputHandler(e)}
+            onBlurFunction={(e) => InputCheck(e)}
+          />
+
+          <div className="nombrecampo">Password</div>
+          <InputLabel
+            type="password"
+            classDesign=""
+            name="password"
+            functionHandler={(e) => InputHandler(e)}
+            placeholder="Introduce tu password"
+            onBlurFunction={(e) => InputCheck(e)}
+          />
+          <div className="errorMessageDesign">
+            <p>{errorMessage}</p>
+          </div>
+
+          <Button name="Enviar" path="" functionButton={(e) => logMe(e)} />
+        </div>
+  
+  
+  </>;
+};
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,92 +1,15 @@
-import { useState } from "react";
+import React from "react";
 import "./Home.css";
-import { loginMe, registerMe } from "../../services/apiCall";
-import jwt_decode from "jwt-decode";
-import { InputLabel } from "../../common/InputLabel/InputLabel";
-import { CheckError } from "../../services/useful";
-import { Button } from "../../common/Button/Button";
+import { LoginForm } from "../../common/LoginForm/LoginForm";
+
 
 export const Home = () => {
-  //CHECK FIELDS
-  const [errorMessage, setErrorMessage] = useState("");
-  const [credentialsError, setCredentialsError] = useState({
-    emailError: "",
-    passwordError: "",
-  });
 
-  const InputCheck = (e) => {
-    let mensajeError = CheckError(e.target.name, e.target.value);
-    console.log("Esto son las credenciatlsError");
-    console.log(credentialsError);
-    setCredentialsError((prevState) => ({
-      ...prevState,
-      [e.target.name + "Error"]: mensajeError,
-    }));
-  };
-
-  //LOGIN
-  const [credentials, setCredentials] = useState({
-    email: "",
-    password: "",
-  });
-
-  const InputHandler = (e) => {
-    setCredentials((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
-    console.log(credentials);
-  };
-
-  const logMe = (e) => {
-    e.preventDefault();
-
-    loginMe(credentials)
-      .then((resultado) => {
-        let decoded = jwt_decode(resultado.data.token);
-        let datosBackend = {
-          token: resultado.data.token,
-          user: decoded,
-        };
-        console.log(datosBackend);
-        setErrorMessage("");
-      })
-      .catch((error) => {
-        console.log(error.response.data.message);
-        setErrorMessage(error.response.data.message);
-      });
-  };
 
   return (
     <>
       <div className="containerhome">
-        <div className="formulario">
-          <div className="titulo">LOGIN</div>
-          <div className="nombrecampo">Usuario</div>
-          <InputLabel
-            type="text"
-            placeholder="Introduce tu email"
-            name="email"
-            classDesign=""
-            functionHandler={(e) => InputHandler(e)}
-            onBlurFunction={(e) => InputCheck(e)}
-          />
-
-          <div className="nombrecampo">Password</div>
-          <InputLabel
-            type="password"
-            classDesign=""
-            name="password"
-            functionHandler={(e) => InputHandler(e)}
-            placeholder="Introduce tu password"
-            onBlurFunction={(e) => InputCheck(e)}
-          />
-          <div className="errorMessageDesign">
-            <p>{errorMessage}</p>
-          </div>
-
-          <Button name="Enviar" path="" functionButton={(e) => logMe(e)} />
-        </div>
+        <LoginForm/>
       </div>
     </>
   );
