e64952081b1268a24931a27b11ecc79576759325
Carlos <carresan.val@gmail.com>
2023-10-16 20:43:20 +0200

    NEW: Formateado Home y Home para movil

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,13 +6,13 @@ export const Home = () => {
   return (
     <>
       <div className="containerhome d-flex justify-content-center align-items-start">
-        <Container className="d-flex m-0 p-0 b-0">
-          <Row className="d-flex w-100 m-0 p-0">
+        <Container className="m-0">
+          <Row className="w-100 m-0 p-0">
             <Col className="slidePhoto col-6"></Col>
-            <Col className="slideDescription col-6 d-flex align-items-center justify-content-center">
-              <div className="text-light text-center h3 h-50 w-100 px-4">
-                {" "}
-                Esto es un texto generico de prueba prueba
+            <Col className="slideDescription col-6 d-flex align-items-center justify-content-center flex-column">
+              <div className="d-flex align-items-start flex-column h-50">
+                <div className="homeTitleDesign"> Carlos Redondo </div>
+                <div className="homeSubtitleDesign">Junior Developer </div>
               </div>
             </Col>
           </Row>
