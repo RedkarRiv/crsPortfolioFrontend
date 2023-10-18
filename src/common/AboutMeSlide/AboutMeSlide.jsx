3f13e8b054f3a8b57038cbb598fcd6918c6cd9fa
Carlos <carresan.val@gmail.com>
2023-10-18 18:04:02 +0200

    NEW: Creadas y formateadas tablas de contenido en slideAboutMe en Home.jsx v2

--- src/common/AboutMeSlide/AboutMeSlide.jsx
+++ src/common/AboutMeSlide/AboutMeSlide.jsx
@@ -5,10 +5,10 @@ import { Col, Row } from "react-bootstrap";
 export const AboutMeSlide = () => {
   return (
     <>
-      <div className="techTitle d-flex text-center justify-content-center pt-5 mt-5">
-        Acerca de mí
-      </div>
-      <Row className="aboutMeBG w-100 m-0 mb-4 p-0 d-flex justify-content-center">
+      <Row className="aboutMeBG m-0 mb-4 p-0 d-flex justify-content-center">
+        <Col className="col-12 techTitle d-flex text-center justify-content-center pt-5">
+          Acerca de mí
+        </Col>
         <Col className="col-12 col-md-4 d-flex justify-content-center align-items-start pt-4">
           <div className="techLabel"></div>
         </Col>
@@ -17,6 +17,9 @@ export const AboutMeSlide = () => {
         </Col>{" "}
         <Col className="col-12 col-md-4 d-flex justify-content-center align-items-start pt-4">
           <div className="techLabel"></div>
+        </Col>
+        <Col className="col-12 downloadFilesContainer d-flex justify-content-center align-items-center">
+          <div className="fileDownload">Curriculum Vitae</div>
         </Col>
       </Row>
     </>
