1bba904f46c4f7fb055dd493d6904d9374cba524
Carlos <carresan.val@gmail.com>
2023-10-19 02:10:45 +0200

    FIX: Reformatedo diseño de slideMyProjects para versión movil

--- src/common/MyProjects/MyProjects.jsx
+++ src/common/MyProjects/MyProjects.jsx
@@ -5,15 +5,36 @@ import { Col, Row } from "react-bootstrap";
 export const MyProjects = () => {
   return (
     <>
-      <Row className="myProjectsBG d-flex m-0 py-5">
+      <Row className="myProjectsBG d-flex m-0 px-3 py-5">
+        <Col className="col-12 techTitle d-flex text-center justify-content-center p-0 px-0 m-0">
+          Mis proyectos{" "}
+        </Col>
         <Col className="col-12 m-0 p-0 d-flex justify-content-start align-items-center flex-column">
-          <Row className="d-flex w-75 h-50 my-2">
-            <Col className="col-12 col-md-6 projectImage">IMAGEN</Col>
-            <Col className="col-12 col-md-6 projectDescription">DESCRIPCION</Col>
+          <Row className="d-flex my-2 projectSlide">
+            <Col className="col-12 col-md-6 projectImage m-0 p-0">IMAGEN</Col>
+            <Col className="col-12 col-md-6 projectDescription m-0 p-0">
+              DESCRIPCION
+            </Col>
           </Row>
-          <Row className="d-flex w-75 h-50 my-2">
-            <Col className="col-12 col-md-6 projectImage">IMAGEN</Col>
-            <Col className="col-12 col-md-6  projectDescription">DESCRIPCION</Col>
+          <Row className="d-flex my-2 projectSlide">
+            <Col className="col-12 col-md-6 projectImage m-0 p-0">IMAGEN</Col>
+            <Col className="col-12 col-md-6  projectDescription m-0 p-0">
+              DESCRIPCION
+            </Col>
+          </Row>
+        </Col>
+        <Col className="col-12 m-0 p-0 d-flex justify-content-start align-items-center flex-column">
+          <Row className="d-flex my-2 projectSlide">
+            <Col className="col-12 col-md-6 projectImage m-0 p-0">IMAGEN</Col>
+            <Col className="col-12 col-md-6 projectDescription m-0 p-0">
+              DESCRIPCION
+            </Col>
+          </Row>
+          <Row className="d-flex my-2 projectSlide">
+            <Col className="col-12 col-md-6 projectImage m-0 p-0">IMAGEN</Col>
+            <Col className="col-12 col-md-6 m-0 p-0 projectDescription">
+              DESCRIPCION
+            </Col>
           </Row>
         </Col>
       </Row>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -52,8 +52,7 @@ export const Home = () => {
           </Col>
         </Row>
         <AboutMeSlide />
-      <MyProjects/>
-
+        <MyProjects />
       </Container>
     </>
   );
