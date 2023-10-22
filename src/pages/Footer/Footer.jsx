fd036bf324c239f0adc1405e205b90b6e5fa24f0
Carlos <carresan.val@gmail.com>
2023-10-23 01:08:38 +0200

    FIX: Cambios de diseño en footer y hipervinculadas rrss

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,37 +1,34 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import "./Footer.css";
+import gitLogo from "../../img/gitLogo.png";
+import linkedinLogo from "../../img/linkedinLogo.png";
 
 const Footer = () => (
   <footer className="page-footer font-small blue py-3 footerBG">
-    <Container fluid className="d-flex justify-content-around align-items-center">
+    <Container
+      fluid
+      className="d-flex justify-content-around align-items-center"
+    >
       <Row className="p-0 m-0 w-100 d-flex justify-content-around align-items-center">
-        <Col className="col-md-4 mt-md-0 d-flex justify-content-center align-items-center">
-          <ul className="list-unstyled d-flex flex-column m-0">
-            <li>
-              <div>Aviso legal</div>
-            </li>
-            <li>
-              <div>Política de privacidad</div>
-            </li>
-          </ul>
-        </Col>
         <hr className="clearfix w-100 d-md-none pb-0" />
-        <Col className="col-md-4 mb-md-0 mb-3 d-flex justify-content-center align-items-center">
-          <div>
-            <div>Designed by </div>
-            <h5 className="text-uppercase">Carlos Redondo</h5>
-          </div>
+        <Col className="col-12 col-md-4 mb-md-0 mb-3 d-flex justify-content-center align-items-center">
+          <div className="me-3">Designed by </div>
+          <h5 className="text-uppercase m-0">Carlos Redondo</h5>
         </Col>
         <Col className="col-md-3 mb-md-0 d-flex justify-content-center align-items-center">
-          <ul className="list-unstyled m-0 text-center">
-            <li>
-              <div>Linkedin</div>
-            </li>
-            <li>
-              <div>GitHub</div>
-            </li>
-          </ul>
+          <div className="d-flex m-0 text-center w-50 justify-content-around">
+            <div className="d-flex justify-content-center align-items-center">
+              <a href="https://github.com/RedkarRiv" target="_blank">
+                <img src={gitLogo} className="logoResize p-1" />
+              </a>
+            </div>
+            <div className="d-flex justify-content-center align-items-center">
+              <a href="https://www.linkedin.com/in/crs5" target="_blank">
+                <img src={linkedinLogo} className="logoResize2" />
+              </a>
+            </div>
+          </div>
         </Col>
       </Row>
     </Container>
