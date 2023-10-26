8cda9c1c4eae5699ab7b36757a5cf3d63ce69d2e
Carlos <carresan.val@gmail.com>
2023-10-26 19:13:32 +0200

    NEW: Abstraido projectsPortfolio a data/projectData.js e importado en componente correctamente

--- src/common/MyProjects/MyProjects.jsx
+++ src/common/MyProjects/MyProjects.jsx
@@ -1,55 +1,8 @@
 import React from "react";
 import "./MyProjects.css";
 import { Col, Row } from "react-bootstrap";
-import dentalClinicFront from "../../img/dentalClinicFront_img.png";
-import dentalClinicBack from "../../img/dentalClinicBack_img.png";
-import MasterMind from "../../img/MasterMind_img.png";
-import MasterMindMobile from "../../img/MasterMind_img_mobile.png";
-import Devstaurante from "../../img/Devstaurante_img.png";
-import DevstauranteMobile from "../../img/Devstaurante_img_mobile.png";
-import GBColor from "../../img/GBC_img.png";
 import { Button } from "../Button/Button";
-import WWSimg from "../../img/WWS_img_rs.png";
-import WWSimg_mb from "../../img/WWS_img_mobile_rs.png";
-
-const projectsPortfolio = [
-  {
-    title: "WWS helpdesk",
-    imgPC: WWSimg,
-    imgMobile: WWSimg_mb,
-    repo: "https://github.com/RedkarRiv/ClinicaDental_BackEnd_CRS",
-  },
-  {
-    title: "Dental Clinic Backend",
-    imgPC: dentalClinicBack,
-    imgMobile: undefined,
-    repo: "https://github.com/RedkarRiv/REACT_DentalClinic",
-  },
-  {
-    title: "Dental Clinic Frontend",
-    imgPC: dentalClinicFront,
-    imgMobile: undefined,
-    repo: "https://github.com/RedkarRiv/ClinicaDental_BackEnd_CRS",
-  },
-  {
-    title: "Devstaurante",
-    imgPC: Devstaurante,
-    imgMobile: DevstauranteMobile,
-    repo: "https://github.com/RedkarRiv/Rest_Project",
-  },
-  {
-    title: "MasterMind",
-    imgPC: MasterMind,
-    imgMobile: MasterMindMobile,
-    repo: "https://github.com/RedkarRiv/MasterMind",
-  },
-  {
-    title: "Game Boy Color SIM",
-    imgPC: GBColor,
-    imgMobile: undefined,
-    repo: "https://github.com/RedkarRiv/GH_FSD_GBC",
-  },
-];
+import projectsPortfolio  from "../../data/projectsData.js"
 
 export const MyProjects = ({ id }) => {
   return (
@@ -67,7 +20,7 @@ export const MyProjects = ({ id }) => {
               return (
                 <Col
                   className="col-12 col-xl-6  m-0 p-2 d-flex justify-content-between align-items-center flex-column"
-                  id={item.title}
+                  key={item.title}
                 >
                   <div className="projectContainer d-flex justify-content-center align-items-center flex-column p-4">
                     <div className="projectTitle">{item.title}</div>
---
+++ src/data/projectsData.js
@@ -0,0 +1,53 @@
+
+import dentalClinicFront from "../img/dentalClinicFront_img.png";
+import dentalClinicBack from "../img/dentalClinicBack_img.png";
+import MasterMind from "../img/MasterMind_img.png";
+import MasterMindMobile from "../img/MasterMind_img_mobile.png";
+import Devstaurante from "../img/Devstaurante_img.png";
+import DevstauranteMobile from "../img/Devstaurante_img_mobile.png";
+import GBColor from "../img/GBC_img.png";
+import WWSimg from "../img/WWS_img_rs.png";
+import WWSimg_mb from "../img/WWS_img_mobile_rs.png";
+
+
+const projectsPortfolio = [
+    {
+      title: "WWS helpdesk",
+      imgPC: WWSimg,
+      imgMobile: WWSimg_mb,
+      repo: "https://github.com/RedkarRiv/ClinicaDental_BackEnd_CRS",
+    },
+    {
+      title: "Dental Clinic Backend",
+      imgPC: dentalClinicBack,
+      imgMobile: undefined,
+      repo: "https://github.com/RedkarRiv/REACT_DentalClinic",
+    },
+    {
+      title: "Dental Clinic Frontend",
+      imgPC: dentalClinicFront,
+      imgMobile: undefined,
+      repo: "https://github.com/RedkarRiv/ClinicaDental_BackEnd_CRS",
+    },
+    {
+      title: "Devstaurante",
+      imgPC: Devstaurante,
+      imgMobile: DevstauranteMobile,
+      repo: "https://github.com/RedkarRiv/Rest_Project",
+    },
+    {
+      title: "MasterMind",
+      imgPC: MasterMind,
+      imgMobile: MasterMindMobile,
+      repo: "https://github.com/RedkarRiv/MasterMind",
+    },
+    {
+      title: "Game Boy Color SIM",
+      imgPC: GBColor,
+      imgMobile: undefined,
+      repo: "https://github.com/RedkarRiv/GH_FSD_GBC",
+    },
+  ];
+  
+
+  export default projectsPortfolio;
