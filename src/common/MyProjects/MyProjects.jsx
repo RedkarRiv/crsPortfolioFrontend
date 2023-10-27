fedcb13172f2a2f781b6b268c68dc903bf4bbac1
Carlos <carresan.val@gmail.com>
2023-10-27 12:45:12 +0200

    FIX: Nuevo ajuste de diseño para MyProjects en Home

--- src/common/MyProjects/MyProjects.jsx
+++ src/common/MyProjects/MyProjects.jsx
@@ -10,20 +10,20 @@ export const MyProjects = ({ id }) => {
       id={id}
       className="myProjectsBG d-flex justify-content-center p-0 mt-3"
     >
-      <Row className="myProjectsSlideDesign d-flex justify-content-center m-0 pt-5 pb-3 w-100">
+      <Row className="myProjectsSlideDesign d-flex justify-content-center m-0 pt-5 pb-3">
         <Col className="col-12 fs-1 fw-bold d-flex text-center text-light justify-content-center align-items-center py-3 px-0 mt-3">
           Mis proyectos{" "}
         </Col>
         <Col className="col-12 m-0 p-0 d-flex justify-content-start align-items-center flex-column">
-          <Row className="d-flex my-2 w-75 justify-content-around">
+          <Row className="d-flex my-2 justify-content-around px-1 w-75">
             {projectsPortfolio.map((item) => {
               return (
                 <Col
-                  className="col-12 col-xl-6  m-0 p-2 d-flex justify-content-between align-items-center flex-column"
+                  className="col-12 col-xl-5 mt-3 p-2 d-flex justify-content-between align-items-center flex-column"
                   key={item.title}
                 >
-                  <div className="d-flex justify-content-center align-items-center flex-column p-4">
-                    <div className="fs-4 p-1 fw-bold  text-light ">{item.title}</div>
+                  <div className="projectContainer rounded d-flex justify-content-center align-items-center flex-column p-4">
+                    <div className="fs-4 p-1 fw-bold  text-dark ">{item.title}</div>
                     <div className="w-50 d-flex justify-content-around align-items-center my-3">
                       <div className="techElement"></div>
                       <div className="techElement"></div>
